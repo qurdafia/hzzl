a5481449ac203536f9d49c3552957c9c290b6b11
Mark Christian Roma <markchristianroma@Marks-iMac.local>
2022-10-01 19:46:52 +0800

    Tweaking font color.

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -2,9 +2,7 @@
 const InputComponentStyle = {
         border: "none",
         boxSizing: "border-box",
-        color: "#666666",
         fontSize:  "16px",
-        // marginTop: "20px",
         padding: "15px",
         width: "100%"
 }
